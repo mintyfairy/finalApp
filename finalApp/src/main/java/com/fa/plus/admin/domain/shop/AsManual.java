5e161236233cd6eaf2e126a10a7dbc3ce4c58907
gmlwj <gmlwj@DESKTOP-C2QEQ2J>
2023-12-20 15:11:29 +0900

    쇼핑몰 공지사항관리자, 공지사항일반, 문의글일반, AS문의글 일반,AS문의글 관리자, AS매뉴얼 관리자

---
+++ finalApp/src/main/java/com/fa/plus/admin/domain/shop/AsManual.java
@@ -0,0 +1,145 @@
+package com.fa.plus.admin.domain.shop;
+
+public class AsManual {
+	private long manualNum;
+	private String userId;
+	private String userName;
+	private String subject;
+	private String content;
+	private String reg_date;
+	private int hitCount;
+	private int notice;
+	private int showNotice;
+
+	private long fileNum;
+	private String originalFilename;
+	private String saveFilename;
+	private long fileSize;
+	private int fileCount;
+		
+	//private List<MultipartFile> selectFile;  
+	private long gap;
+
+	public long getManualNum() {
+		return manualNum;
+	}
+
+	public void setManualNum(long manualNum) {
+		this.manualNum = manualNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+
+	public int getHitCount() {
+		return hitCount;
+	}
+
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+
+	public int getNotice() {
+		return notice;
+	}
+
+	public void setNotice(int notice) {
+		this.notice = notice;
+	}
+
+	public int getShowNotice() {
+		return showNotice;
+	}
+
+	public void setShowNotice(int showNotice) {
+		this.showNotice = showNotice;
+	}
+
+	public long getFileNum() {
+		return fileNum;
+	}
+
+	public void setFileNum(long fileNum) {
+		this.fileNum = fileNum;
+	}
+
+	public String getOriginalFilename() {
+		return originalFilename;
+	}
+
+	public void setOriginalFilename(String originalFilename) {
+		this.originalFilename = originalFilename;
+	}
+
+	public String getSaveFilename() {
+		return saveFilename;
+	}
+
+	public void setSaveFilename(String saveFilename) {
+		this.saveFilename = saveFilename;
+	}
+
+	public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public int getFileCount() {
+		return fileCount;
+	}
+
+	public void setFileCount(int fileCount) {
+		this.fileCount = fileCount;
+	}
+
+	public long getGap() {
+		return gap;
+	}
+
+	public void setGap(long gap) {
+		this.gap = gap;
+	}
+
+	
+	
+}
