6b4aa4eca6aa525dc9c7fa6b87eab883c058ad60
user <user@DESKTOP-0L11UB0>
2023-12-28 17:43:18 +0900

    aa

--- finalApp/src/main/java/com/fa/plus/controller/campsite/CampAdminController.java
+++ finalApp/src/main/java/com/fa/plus/controller/campsite/CampAdminController.java
@@ -1,11 +1,18 @@
 package com.fa.plus.controller.campsite;
 
+import java.io.File;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fa.plus.domain.site.Site;
+import com.fa.plus.domain.site.SiteDetail;
 
 @Controller
 @RequestMapping("/siteManage/*")
@@ -20,26 +27,61 @@ public class CampAdminController {
 	@GetMapping("site/write")
 	public String siteForm(Model model) {
 		
+		Site dto=null;
+		
+		model.addAttribute("dto", dto);
 		model.addAttribute("mode", "write");
 		return ".campsite.siteWrite";
 	}
 	@PostMapping("site/write")
-	public String siteSubmit() {
+	public String siteSubmit(Site dto,HttpSession session,
+			Model model) {
+		String root = session.getServletContext().getRealPath("/");
+		String path = root + "uploads" + File.separator + "product";
+		/*
+		try {
+			service.insertProduct(dto, path);
+		} catch (Exception e) {
+		}
 		
+		String url = "redirect:/admin/product/main?parentNum=" + dto.getParentNum()
+						+ "&categoryNum=" + dto.getCategoryNum();
+		if(special != 0) {
+			url += "&special=" + special;
+		}
+		*/
 		
-		return ".campsite.siteWrite";
+		return ".admin.siteManage.roomList.1";
 	}
 	@GetMapping("room/{num}/write")
 	public String roomForm(@PathVariable int num,Model model) {
 		
+		SiteDetail dto=null;
+		
+		model.addAttribute("dto", dto);
 		model.addAttribute("mode", "write");
-		return ".campsite.roomWrite";
+		return ".campsite.";
 	}
 	@PostMapping("room/{num}/write")
-	public String roomSubmit(@PathVariable int num) {
+	public String roomSubmit(@PathVariable int num,SiteDetail dto,HttpSession session,
+			Model model) {
+		String root = session.getServletContext().getRealPath("/");
+		String path = root + "uploads" + File.separator + "product";
+		/*
+		try {
+			service.insertProduct(dto, path);
+		} catch (Exception e) {
+		}
+		
+		String url = "redirect:/admin/product/main?parentNum=" + dto.getParentNum()
+						+ "&categoryNum=" + dto.getCategoryNum();
+		if(special != 0) {
+			url += "&special=" + special;
+		}
+		*/
 		
 		
-		return ".campsite.roomWrite";
+		return ".admin.siteManage.siteManage";
 	}
 	
 
